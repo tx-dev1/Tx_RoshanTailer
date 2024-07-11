3f3678af6515af7a1517621bcbe4b35c20ddd841
om <om.prakash@linked.exchange>
2024-07-12 00:10:12 +0530

    user registration, login code pushed

--- TestRoshanTailor/Controllers/HomeController.cs
+++ TestRoshanTailor/Controllers/HomeController.cs
@@ -1,16 +1,114 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using TestRoshanTailor.Models;
 
 namespace TestRoshanTailor.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        //      [HttpPost]
+        //      public async Task<IActionResult> Index(RegisterViewModel model)
+        //      {
+        //	if (ModelState.IsValid)
+        //	{
+        //		var result = await RegisterUser(model);
+        //		if (result > 0)
+        //		{
+        //			// Registration successful, redirect to login or home page
+        //			return (IActionResult)RedirectToAction("Contact", "Home");
+        //		}
+        //		else if (result == -1)
+        //		{
+        //			ModelState.AddModelError(string.Empty, "Username already exists.");
+        //		}
+        //		else if (result == -2)
+        //		{
+        //			ModelState.AddModelError(string.Empty, "Email already exists.");
+        //		}
+        //		else
+        //		{
+        //			ModelState.AddModelError(string.Empty, "Registration failed. Please try again.");
+        //		}
+        //	}
+
+        //	return (IActionResult)View(model);
+        //}
+
+
+
+        [HttpPost]
+        public ActionResult Index(RegisterViewModel model)
+        {
+
+            var result = RegisterUser(model);
+            if (result.Result > 0)
+            {
+                // Registration successful, redirect to login or home page
+                return RedirectToAction("Contact", "Home");
+            }
+            else if (result.Result == -1)
+            {
+                ModelState.AddModelError(string.Empty, "Username already exists.");
+            }
+            else if (result.Result == -2)
+            {
+                ModelState.AddModelError(string.Empty, "Email already exists.");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Registration failed. Please try again.");
+            }
+
+            return View(model);
+
+        }
+        private async Task<int> RegisterUser(RegisterViewModel model)
+        {
+            int result = 0;
+            using (rosharxk_Entities rx = new rosharxk_Entities())
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("rosharxk_Entities")))
+                {
+                    using (var command = new SqlCommand("SP_UserRegistration", connection))
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Username", model.Username);
+                        command.Parameters.AddWithValue("@Password", model.Password);
+                        command.Parameters.AddWithValue("@Email", model.Email);
+
+                        await connection.OpenAsync();
+                        result = (int)await command.ExecuteScalarAsync();
+                    }
+                }
+
+                return result;
+            }
         }
 
         public ActionResult About()
@@ -24,6 +122,6 @@ namespace TestRoshanTailor.Controllers
         {
             return View();
         }
-        
+
     }
 }
---
+++ TestRoshanTailor/Models/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TestRoshanTailor.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Username cannot be longer than 20 characters.")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+        public string Password { get; set; }
+
+		[NotMapped]
+		[Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        //public bool AcceptTerms { get; set; }
+    }
+}
